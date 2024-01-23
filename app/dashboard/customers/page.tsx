196cb9e74e141388e3e97596fd0ca5bdf0e4b2ec
bandeiraos <pedrocargninbandeira@gmail.com>
2024-01-23 10:53:35 -0300

    chap 16 - adding metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
 export default function Page() {
-    return <p>CUSTOMERS Page</p>
+    return <p>CUSTOMERS Page</p>;
 }
